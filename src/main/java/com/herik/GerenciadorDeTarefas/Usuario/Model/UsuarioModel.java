c0e908dfd8bd596fbc0eab702d6446a55296e6c7
HerikMario <herikmariomr@gmail.com>
2025-06-05 13:49:56 -0300

    refactor: Adiciona @Column nos atributos

--- src/main/java/com/herik/GerenciadorDeTarefas/Usuario/Model/UsuarioModel.java
+++ src/main/java/com/herik/GerenciadorDeTarefas/Usuario/Model/UsuarioModel.java
@@ -14,12 +14,16 @@ public class UsuarioModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "nome")
     private String nome;
 
+    @Column(name = "email")
     private String email;
 
+    @Column(name = "tarefas")
     @OneToMany(mappedBy = "usuario")
     private List<TarefaModel> tarefas;
 
