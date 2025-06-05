15119e9db7a34a0b3b7665d26cbc59c8e4a39bd0
HerikMario <herikmariomr@gmail.com>
2025-06-05 13:53:54 -0300

    feat: Adiciona camada DTO para filtrar a entrada da forma que o programa precisa

--- src/main/java/com/herik/GerenciadorDeTarefas/Usuario/DTO/UsuarioDTO.java
+++ src/main/java/com/herik/GerenciadorDeTarefas/Usuario/DTO/UsuarioDTO.java
@@ -1,4 +1,12 @@
 package com.herik.GerenciadorDeTarefas.Usuario.DTO;
 
+import java.util.List;
+
 public class UsuarioDTO {
+
+    private String nome;
+    private String email;
+    private int idade;
+    private List<Long> id_tarefas;
+
 }
